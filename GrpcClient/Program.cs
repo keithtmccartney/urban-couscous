2583d945ec55fc7f042039427f7507f3837f2b9f
Keith McCartney <keithtmccartney@hotmail.com>
2022-12-04 21:33:25 +0000

    Establishing initial client/server connection.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7063");
+
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new GrpcService.HelloRequest { Name = "gRPC" });
+
+Console.WriteLine(string.Format("From Server: {0}", reply));
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
