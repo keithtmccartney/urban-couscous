427eb4a61babaa623eefb514e7ac0a5c7cf26895
Keith McCartney <keithtmccartney@hotmail.com>
2022-12-05 21:53:39 +0000

    Adding a Sales Data Service and deadline exception handling.

---
+++ GrpcService/Services/SalesDataService.cs
@@ -0,0 +1,63 @@
+﻿using Grpc.Core;
+using System.Threading;
+
+namespace GrpcService.Services
+{
+    public class SalesDataService : SalesService.SalesServiceBase
+    {
+        public override async Task GetSalesData(Request request, IServerStreamWriter<SalesDataModel> responseStream, ServerCallContext context)
+        {
+            using (var reader = new StreamReader(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", @"C:\Users\Keith\Downloads\5m-Sales-Records\5m Sales Records.csv")))
+            {
+                string line;
+
+                bool isFirstLine = true;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var pieces = line.Split(',');
+
+                    var _model = new SalesDataModel();
+
+                    try
+                    {
+                        if (isFirstLine)
+                        {
+                            isFirstLine = false;
+
+                            continue;
+                        }
+
+                        _model.Region = pieces[0];
+
+                        _model.Country = pieces[1];
+
+                        _model.OrderID = int.TryParse(pieces[6], out int _orderID) ? _orderID : 0;
+
+                        _model.UnitPrice = float.TryParse(pieces[9], out float _unitPrice) ? _unitPrice : 0;
+
+                        _model.ShipDate = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime((DateTime.TryParse(pieces[7], out DateTime _dateShip) ? _dateShip : DateTime.MinValue).ToUniversalTime());
+
+                        _model.UnitsSold = int.TryParse(pieces[8], out int _unitsSold) ? _unitsSold : 0;
+
+                        _model.UnitCost = float.TryParse(pieces[10], out float _unitCost) ? _unitCost : 0;
+
+                        _model.TotalRevenue = int.TryParse(pieces[11], out int _totalRevenue) ? _totalRevenue : 0;
+
+                        _model.TotalCost = int.TryParse(pieces[13], out int _totalCost) ? _totalCost : 0;
+
+                        await responseStream.WriteAsync(_model);
+                    }
+                    /*catch (Exception ex)
+                    {
+                        throw new RpcException(new Status(StatusCode.Internal, ex.ToString()));
+                    }*/
+                    catch (RpcException ex) when (ex.StatusCode == StatusCode.DeadlineExceeded)
+                    {
+                        Console.WriteLine("Service timeout.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SalesDataClient/Program.cs
@@ -0,0 +1,29 @@
+﻿//Create a channel for your gRPC Service
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7063");
+
+//Create SalesService Client to open a connection
+var client = new SalesService.SalesServiceClient(channel);
+
+//Invoke the method
+using var call = client.GetSalesData(new Request { Filters = "" }, deadline: DateTime.UtcNow.AddSeconds(15));
+
+int Count = 0;
+
+//Get response stream
+await foreach (var each in call.ResponseStream.ReadAllAsync())
+{
+    Console.WriteLine(String.Format("New Order received from {0}-{1} / Order ID = {2} / Unit Price = {3} / Ship Date = {4}", each.Country, each.Region, each.OrderID, each.UnitPrice, each.ShipDate));
+
+    Count++;
+}
+
+Console.WriteLine("Stream ended, total records: " + Count.ToString());
+
+Console.Read();
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
